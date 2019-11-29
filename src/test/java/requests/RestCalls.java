ca54650b590a9eceb54c3704012624c23873c19c
Andrii <apapmind@gmail.com>
2019-11-29 13:35:59 +0100

    Added RestCalls.java file

---
+++ src/test/java/requests/RestCalls.java
@@ -0,0 +1,38 @@
+package requests;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class RestCalls {
+
+    public static Response getRequest(String uri) {
+        RequestSpecification requestSpecification = RestAssured.given();
+        requestSpecification.contentType(ContentType.JSON);
+        Response response = requestSpecification.get(uri);
+        return response;
+    }
+
+    public static Response postRequest(String uri, String jsonPayload) {
+        RequestSpecification requestSpecification = RestAssured.given().body(jsonPayload);
+        requestSpecification.contentType(ContentType.JSON);
+        Response response = requestSpecification.post(uri);
+
+        return response;
+    }
+
+    public static Response putRequest(String uri, String jsonPayload) {
+        RequestSpecification requestSpecification = RestAssured.given().body(jsonPayload);
+        requestSpecification.contentType(ContentType.JSON);
+        Response response = requestSpecification.put(uri);
+        return response;
+    }
+
+    public static Response deleteRequest(String uri, String jsonPayload) {
+        RequestSpecification requestSpecification = RestAssured.given().body(jsonPayload);
+        requestSpecification.contentType(ContentType.JSON);
+        Response response = requestSpecification.delete(uri);
+        return response;
+    }
+}
